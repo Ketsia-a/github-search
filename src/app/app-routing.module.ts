095bf30b1d7ee7b5591994fe263a7c546738b5a3
Ketsia-a <iraketsia2001@gmail.com>
2020-11-16 05:53:27 +0200

    added routers but component doesn't work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from './user/user.component';
+import {RepoComponent} from './repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'User', component:'./user/user.component'},
+ { path: 'Repo', component: './repo/repo.component'},
+ {path:'', redirectTo:'/User', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
