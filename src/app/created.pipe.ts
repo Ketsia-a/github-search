d624999da307e8bb3fd94010c4af4fc425010709
Ketsia-a <iraketsia2001@gmail.com>
2020-11-16 08:43:53 +0200

    created a custom pipe

---
+++ src/app/created.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'created'
+})
+export class CreatedPipe implements PipeTransform {
+  transform(value: any): any {
+    let year: any = new Date().getFullYear();
+    let yearOfCreation: any = new Date(value).getFullYear();
+    let git = year - yearOfCreation;
+    return git;
+  }
+}
