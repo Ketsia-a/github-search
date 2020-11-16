8b94a184531f6c65d7a4144dec354a449a2c0fc1
Ketsia-a <iraketsia2001@gmail.com>
2020-11-16 05:47:08 +0200

    added repo request

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GitserverService } from '../gitserver.service';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-repo',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  repo: Repository[]
+  constructor( private repositoryService:GitserverService) { }
+  reposearch(textsearch){
+    this. repositoryService.RepoRequest(textsearch).then(
+      ()=>{
+        this.repo=this.repositoryService.repository;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
   }
-
+  ngOnInit(): void {
+    this.repositoryService.RepoRequest("")
+    this.repo = this.repositoryService.repository
+  }
 }
